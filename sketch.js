26ad7f279be94ab19696b6990d00bd7ec7820d8e
Julia <shaifelejulia@gmail.com>
2022-10-15 16:21:55 +0400

    refactoring, new feature

--- sketch.js
+++ sketch.js
@@ -1,72 +1,50 @@
 let bgColor = [0,254];
-var rectangle1 = {
-		posX: getRandomArbitrary(0,800),
-		posY: getRandomArbitrary(0,800),
-		size: getRandomArbitrary(50, 150)
-	}
-var rectangle2 = {
-		posX: getRandomArbitrary(0,800),
-		posY: getRandomArbitrary(0,800),
-		size: getRandomArbitrary(50, 150)
-	}
-var rectangle3 = {
-		posX: 600,
-		posY: 600,
-		size: getRandomArbitrary(50, 150)
-	}
+let rects = []
 
+for (step = 0; step < 40; step++) {
+    let tempRect = {posX:getRandomArbitrary(0, 1200), 
+        posY:getRandomArbitrary(0, 800), 
+        size: getRandomArbitrary(50, 150)}
+    rects.push(tempRect);
+}
+
+//FUNCTIONS
 function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+    return Math.random() * (max - min) + min;
 }
 
 function setup() {
-	createCanvas(800, 800);
-	frameRate(35);
+    createCanvas(1200, 800);
+    frameRate(35);
+}
+
+function goRandomPath(recrec){
+    recrec.posX = random(1200);
+    recrec.posY = random(800);
+    recrec.size = random(0,150);
 }
 
 function draw() {
-	var bc = random(bgColor);
-	
-	background(bc);
-	
-	if (bc == 0) { 
-		fill(254);
-	} else {
-		fill(0);
-	}	
-	
-	rectangle1.size -= 10; 
-	rectangle1.posX += 5; 
-	rectangle1.posY += 5; 
-	
-	if (rectangle1.size <= 0) { 
-		rectangle1.posX = random(800);
-		rectangle1.posY = random(800);
-		rectangle1.size = random(0,150);
-	}
-	
-	rectangle2.size -= 10; 
-	rectangle2.posX += 5; 
-	rectangle2.posY += 5; 
-	
-	if (rectangle2.size <= 0) { 
-		rectangle2.posX = random(800);
-		rectangle2.posY = random(800);
-		rectangle2.size = random(0,150);
-	}
-	
-	rectangle3.size -= 10; 
-	rectangle3.posX += 5; 
-	rectangle3.posY += 5; 
-	
-	if (rectangle3.size <= 0) { 
-		rectangle3.posX = random(800);
-		rectangle3.posY = random(800);
-		rectangle3.size = random(0,150);
-	}
-	
-	rect(rectangle1.posX,rectangle1.posY,rectangle1.size); 
-	rect(rectangle2.posX,rectangle2.posY,rectangle2.size); 
-	rect(rectangle3.posX,rectangle3.posY,rectangle3.size); 
 
+    var bc =  random(bgColor)
+    background(random(bgColor));
+    noStroke();
+
+    if (bc == 0) { 
+        fill(254);
+    } else {
+        fill(0);
+    }    
+    
+    for (let i = 0; i < rects.length; i += 1) {
+        rects[i].size -= 10;
+        rects[i].posX += 5;
+		rects[i].posY += 5;
+
+        if(rects[i].size <= 0){
+            goRandomPath(rects[i])
+        }
+
+        rect(rects[i].posX, rects[i].posY, rects[i].size);
+    }
 }
